0524d4b020fc4750210f174be08c0c8c6d61ac77
ls <itiapp@163.com>
2017-01-12 13:19:09 +0800

    提交

--- HttpManager/HttpManager/ViewController.m
+++ HttpManager/HttpManager/ViewController.m
@@ -12,6 +12,12 @@
 
 @interface ViewController ()
 
+
+@property(nonatomic,strong) NSData *data;
+
+@property(nonatomic,strong) NSURLSessionDownloadTask *task;
+@property (weak, nonatomic) IBOutlet UIButton *pauseButton;
+
 @end
 
 @implementation ViewController
@@ -25,7 +31,7 @@
 }
 
 - (IBAction)click:(id)sender {
-    
+
     [HttpManager POSTWithURLString:@"http://101.201.78.184:8005/carrierAPI/snsInfoAction/queryBanner" parameters:nil success:^(NSDictionary *response) {
         NSLog(@"%@",response);
     } failure:^(NSError *error) {
@@ -33,6 +39,37 @@
     }];
 
 }
+
+- (IBAction)download:(id)sender {
+    [self.pauseButton setTitle:@"暂停" forState:UIControlStateNormal];
+    self.task= [HttpManager DownLoadFileWithURLString:@"http://www.itiapp.cn/zhimei/php/driver.ipa" parameters:nil savaPath:nil resumeData:nil progress:^(CGFloat progress) {
+                NSLog(@"下载进度：%f%%",progress);
+            } success:^(NSDictionary *response) {
+        
+            } failure:^(NSError *error) {
+                
+            }];
+}
+- (IBAction)pauseOrResume:(UIButton*)sender {
+    
+    if ([sender.currentTitle isEqualToString:@"暂停"]) {
+        [self.task cancelByProducingResumeData:^(NSData * _Nullable resumeData) {
+            self.data=resumeData;
+        }];
+        [sender setTitle:@"继续" forState:UIControlStateNormal];
+    }else{
+        self.task= [HttpManager DownLoadFileWithURLString:@"http://www.itiapp.cn/zhimei/php/driver.ipa" parameters:nil savaPath:nil resumeData:self.data progress:^(CGFloat progress) {
+            NSLog(@"下载进度：%f%%",progress);
+        } success:^(NSDictionary *response) {
+            
+        } failure:^(NSError *error) {
+            
+        }];
+        [sender setTitle:@"暂停" forState:UIControlStateNormal];
+    }
+    
+}
+
 
 
 @end
